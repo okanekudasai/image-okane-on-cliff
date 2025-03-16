f17afaa4efb764248727de5f8e56cdd2a25698e5
opi6opi6 <okanekudasai6@gmail.com>
2025-03-16 12:41:09 +0900

    init

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,8 @@ pip install -r requirements.txt
 
 SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 echo "배포를 시작합니다..."
+echo pwd
+echo ls
 cd ./image_server
 gunicorn --bind 0.0.0.0:9300 image_server.wsgi:application
 
