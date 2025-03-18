799118da29ccde217914de10665b58d5d7b4840a
opi6opi6 <okanekudasai6@gmail.com>
2025-03-19 06:21:08 +0900

    Dev: 토큰에 따른 분기

--- image_server/image_server/views.py
+++ image_server/image_server/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 import os
 from django.views.decorators.csrf import csrf_exempt
+import requests
+from dotenv import load_dotenv
 
 if os.name == "nt":  # Windows
     SAVE_DIR = os.path.join(os.path.expanduser("~"), "Desktop", "images")
@@ -26,27 +28,53 @@ def saveUploadedImage(image_file, filename):
         print(f"이미지 저장 실패: {e}")
         return None
     
+def checkAdmin(accessToken, refreshToken):
+    print(accessToken)
+    print(refreshToken)
+    headers = {
+        'Cookie': f'access={accessToken}; refresh={refreshToken}'
+    }
+    try:
+        # 스프링 서버로 요청 보내기
+        load_dotenv()
+        springUrl = os.getenv("API_HOST")
+        print(springUrl + "admin/test")
+        response = requests.get(springUrl + "admin/test", headers=headers)
+
+        if response.status_code == 200:
+            return "success"
+        else:
+            return "not_admin"
+    except requests.exceptions.RequestException as e:
+        return "request_fail"
+
 @csrf_exempt
 def uploadImage(request):
     if request.method != "POST":
         return JsonResponse({"message": "잘못된 요청"}, status=400)
     
-    folderName = request.POST.get("folderName")
-    if not folderName:
-        return JsonResponse({"message": "폴더명이 필요합니다."}, status=400)
-    saveDir = os.path.join(SAVE_DIR, folderName)
-    os.makedirs(saveDir, exist_ok=True)
-    files = request.FILES.getlist("images")
-    if not files:
-        return JsonResponse({"message": "이미지가 없습니다."}, status=400)
-    savedFiles = []
-    for index, file in enumerate(files, start=1):
-        file_path = os.path.join(saveDir, f"{index}.jpg")
+    isAdmin = checkAdmin(request.COOKIES.get("access"), request.COOKIES.get("refresh"))
+    if isAdmin == "success":
+        folderName = request.POST.get("folderName")
+        if not folderName:
+            return JsonResponse({"message": "폴더명이 필요합니다."}, status=400)
+        saveDir = os.path.join(SAVE_DIR, folderName)
+        os.makedirs(saveDir, exist_ok=True)
+        files = request.FILES.getlist("images")
+        if not files:
+            return JsonResponse({"message": "이미지가 없습니다."}, status=400)
+        savedFiles = []
+        for index, file in enumerate(files, start=1):
+            file_path = os.path.join(saveDir, f"{index}.jpg")
 
-        # 파일을 디스크에 저장
-        with open(file_path, "wb") as dest:
-            for chunk in file.chunks():
-                dest.write(chunk)
+            # 파일을 디스크에 저장
+            with open(file_path, "wb") as dest:
+                for chunk in file.chunks():
+                    dest.write(chunk)
 
-        savedFiles.append(file_path)
-    return JsonResponse({"message": "이미지 저장 성공", "saved_files": savedFiles})
+            savedFiles.append(file_path)
+        return JsonResponse({"message": "이미지 저장 성공", "saved_files": savedFiles})
+    elif isAdmin == "not_admin":
+        return JsonResponse({"message": "권한 없음"})
+    elif isAdmin == "request_fail":
+        return JsonResponse({"message": "서버 실패"})
