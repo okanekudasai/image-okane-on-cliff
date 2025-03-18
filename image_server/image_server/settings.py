681fb527fcad5ea68dd7c05718d940ee171b0f12
opi6opi6 <okanekudasai6@gmail.com>
2025-03-19 07:53:01 +0900

    Dev: cors설정

--- image_server/image_server/settings.py
+++ image_server/image_server/settings.py
@@ -63,9 +63,10 @@ MIDDLEWARE = [
 ]
 
 # cors 설정을 위한 예외 설정
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-# CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'https://okane-on-cliff.kro.kr']
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'https://okane-on-cliff.kro.kr']
+ALLOWED_HOSTS = ['*']
 
 ROOT_URLCONF = 'image_server.urls'
 
